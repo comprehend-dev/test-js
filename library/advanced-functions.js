95e0186a82f8ac676c1a37ae563d0aa20eca2e64
Jonathan Worthington <jnthn@jnthn.net>
2024-02-23 16:04:36 +0100

    Add generator/async methods for testing too

--- library/advanced-functions.js
+++ library/advanced-functions.js
@@ -18,3 +18,26 @@ async function* dribble(interval, ...values) {
         yield value;
     }
 }
+
+class Utils {
+    async awaitAll(...promises) {
+        for (let promise of promises) {
+            await promise;
+        }
+    }
+
+    * evens() {
+        let i = 2;
+        while (true) {
+            yield i;
+            i += 2;
+        }
+    }
+
+    async * dribble(interval, ...values) {
+        for (let value of values) {
+            await new Promise(resolve => setTimeout(resolve, interval));
+            yield value;
+        }
+    }
+}
